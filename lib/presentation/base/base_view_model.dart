6a6cff74143dcbfb367e303f9e6b404344de2f1d
Mark <markmaher933@gmail.com>
2024-05-31 02:48:39 +0300

    OnBoarding View and ViewModel

---
+++ lib/presentation/base/base_view_model.dart
@@ -0,0 +1,12 @@
+abstract class BaseViewModel extends BassViewModelInputs  {
+  
+}
+
+abstract class BassViewModelInputs {
+  void strat();
+  void dispose();
+}
+
+abstract class BassViewModelOutputs {
+  void test();
+}
---
+++ lib/presentation/onboarding/view_model/onboarding_viewmodel.dart
@@ -0,0 +1,26 @@
+import 'package:clean_architecture/presentation/base/base_view_model.dart';
+
+class OnboardingViewModel extends BaseViewModel{
+  @override
+
+  // OnBoarding View Model inputs 
+  void strat() {
+    // TODO: implement strat
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+}
+
+
+abstract class OnboardingViewmodelInputs{
+
+}
+abstract class OnboardingViewmodelOutputs{
+  
+}
+
+
